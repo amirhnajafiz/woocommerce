c987313f269acf565eee4f5bc95599a4dead38e4
9831065 <najafizadeh21@gmail.com>
2021-09-19 11:22:47 +0430

    Order and cart relations added

--- digikala-sample/app/Models/Cart.php
+++ digikala-sample/app/Models/Cart.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 /**
  * Class Cart is the user shopping cart.
@@ -24,4 +26,24 @@ class Cart extends Model
         'user_id',
         'status'
     ];
+
+    /**
+     * Each cart is for a user.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Each cart has its items.
+     *
+     * @return HasMany
+     */
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
+    }
 }
--- digikala-sample/app/Models/Order.php
+++ digikala-sample/app/Models/Order.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 /**
  * Class Order for user orders.
@@ -25,4 +27,24 @@ class Order extends Model
         'item_id',
         'number'
     ];
+
+    /**
+     * Each order belongs to a cart.
+     *
+     * @return BelongsTo
+     */
+    public function cart(): BelongsTo
+    {
+        return $this->belongsTo(Cart::class);
+    }
+
+    /**
+     * Each order has an item.
+     *
+     * @return HasOne
+     */
+    public function item(): HasOne
+    {
+        return $this->hasOne(Item::class);
+    }
 }
