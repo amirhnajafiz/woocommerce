3cc01b2ce6362b2d5114fb87aa4986f2d6a6a0b3
9831065 <najafizadeh21@gmail.com>
2021-09-19 11:35:54 +0430

    All relations added

--- digikala-sample/app/Models/Payment.php
+++ digikala-sample/app/Models/Payment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * Class Payment for user payments.
@@ -25,4 +26,14 @@ class Payment extends Model
         'amount',
         'bank'
     ];
+
+    /**
+     * Each payment is for a cart.
+     *
+     * @return BelongsTo
+     */
+    public function cart(): BelongsTo
+    {
+        return $this->belongsTo(Cart::class);
+    }
 }
