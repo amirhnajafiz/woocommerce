048ef28d272b55929a7f118a9e56345c90b714ca
9831065 <najafizadeh21@gmail.com>
2021-09-19 11:09:49 +0430

    Sale model and migration added

---
+++ digikala-sample/app/Models/Sale.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Sale for sale making.
+ *
+ * @package App\Models
+ */
+class Sale extends Model
+{
+    // Traits
+    use HasFactory;
+
+    /**
+     * Sale fillable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'code',
+        'discount'
+    ];
+}
---
+++ digikala-sample/database/migrations/2021_09_19_063726_create_sales_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', \enum\Limit::NAME())->nullable(false);
+            $table->integer('discount')->default(10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
