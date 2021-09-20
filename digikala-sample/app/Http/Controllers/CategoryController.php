aaf99bea7f4844e7d02929ec50f0f83678ee28e8
9831065 <najafizadeh21@gmail.com>
2021-09-20 09:34:57 +0430

    Category CRUD + Form request added

--- digikala-sample/app/Http/Controllers/CategoryController.php
+++ digikala-sample/app/Http/Controllers/CategoryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateUpdateCategoryRequest;
 use App\Models\Category;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -43,12 +44,22 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param Request $request
-     * @return Response
+     * @param CreateUpdateCategoryRequest $request
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    public function store(CreateUpdateCategoryRequest $request): JsonResponse
     {
-        //
+        $validated = $request->validated();
+        $category = Category::query()->create($validated);
+
+        if ($request->file('file')) {
+            // TODO: Store file in storage and database
+        }
+
+        return \response()->json([
+            'status' => 'OK',
+            'id' => $category->id
+        ]);
     }
 
     /**
@@ -83,13 +94,28 @@ class CategoryController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param Request $request
-     * @param  int  $id
-     * @return Response
+     * @param CreateUpdateCategoryRequest $request
+     * @param int $id
+     * @return JsonResponse
      */
-    public function update(Request $request, $id)
+    public function update(CreateUpdateCategoryRequest $request, int $id): JsonResponse
     {
-        //
+        $category = Category::query()->findOrFail($id);
+        $validated = $request->validated();
+
+        $category->update($validated);
+
+        if ($request->file('file')) {
+            // TODO: Store file in storage and database
+        }
+
+        $category->save();
+
+        return \response()->json([
+            'status' => 'OK',
+            'id' => $category->id,
+            'change' => 'OK'
+        ]);
     }
 
     /**
