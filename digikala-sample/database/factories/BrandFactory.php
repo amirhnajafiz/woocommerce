003b8b09b80f68d4d5828bdf57fc831fbeaf55a6
9831065 <najafizadeh21@gmail.com>
2021-09-20 12:31:52 +0430

    Brand factory added

---
+++ digikala-sample/database/factories/BrandFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Brand;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * Class BrandFactory.
+ *
+ * @package Database\Factories
+ */
+class BrandFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Brand::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->word,
+            'slug' => $this->faker->unique->word,
+            'description' => $this->faker->sentence(7)
+        ];
+    }
+}
