4e5e193d9bbb6771951ea8bf4d87f226fef37098
9831065 <najafizadeh21@gmail.com>
2021-09-21 13:53:01 +0430

    Item controller API reset

--- digikala-sample/app/Http/Controllers/API/ItemController.php
+++ digikala-sample/app/Http/Controllers/API/ItemController.php
@@ -29,35 +29,6 @@ class ItemController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
-     *
-     * @param CreateUpdateItemRequest $request
-     * @return JsonResponse
-     */
-    public function store(CreateUpdateItemRequest $request): JsonResponse
-    {
-        $validated = $request->validated();
-        $item = Item::query()->create($validated);
-
-        if ($request->file('file')) {
-            $name = $item->id . "_image." . $request->file('file')->extension();
-            // Storage storing
-            FileManager::instance()->storeFile('items', $name, $request->file('file'));
-            // Database storing
-            $item->image()->create([
-                'title' => $item->name,
-                'alt' => $item->slug,
-                'path' => 'items/' . $name
-            ]);
-        }
-
-        return \response()->json([
-            'status' => 'OK',
-            'id' => $item->id
-        ]);
-    }
-
-    /**
      * Display the specified resource.
      *
      * @param int $id
@@ -68,58 +39,6 @@ class ItemController extends Controller
         $item = Item::query()->findOrFail($id);
         return \response()->json([
             'item' => $item
-        ]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param CreateUpdateItemRequest $request
-     * @param int $id
-     * @return JsonResponse
-     */
-    public function update(CreateUpdateItemRequest $request, int $id): JsonResponse
-    {
-        $item = Item::query()->findOrFail($id);
-        $validated = $request->validated();
-
-        $item->update($validated);
-
-        if ($request->file('file')) {
-            // Names
-            $oldName = $item->image->path ?? ' ';
-            $name = $item->id . "_image." . $request->file('file')->extension();
-            // Storage update
-            FileManager::instance()->replaceFile('items', $name, $request->file('file'), $oldName);
-            // Database update
-            $item->image()->updateOrCreate([], [
-                'title' => $item->name,
-                'alt' => $item->slug,
-                'path' => 'items/' . $name
-            ]);
-        }
-
-        $item->save();
-
-        return \response()->json([
-            'status' => 'OK',
-            'id' => $item->id,
-            'change' => 'OK'
-        ]);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return JsonResponse
-     */
-    public function destroy(int $id): JsonResponse
-    {
-        Item::query()->findOrFail($id)->delete();
-        return \response()->json([
-            'status' => 'OK',
-            'item' => 'Delete'
         ]);
     }
 }
