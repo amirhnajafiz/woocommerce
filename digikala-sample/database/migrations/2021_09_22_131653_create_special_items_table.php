bd705e3b86a8019f14f47e55655ee3c1b400c0c3
9831065 <najafizadeh21@gmail.com>
2021-09-22 16:50:50 +0330

    Special item migration created

---
+++ digikala-sample/database/migrations/2021_09_22_131653_create_special_items_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+/**
+ * Class CreateSpecialItemsTable.
+ *
+ */
+class CreateSpecialItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('special_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id')
+                ->nullable(false)
+                ->references('id')
+                ->on('items')
+                ->cascadeOnDelete();
+            $table->date('expire_date')->default(now()->addMonth());
+            $table->integer('discount')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('special_items');
+    }
+}
