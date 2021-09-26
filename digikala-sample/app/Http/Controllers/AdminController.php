66157352c115598307af65d8161dcf22342aff25
9831065 <najafizadeh21@gmail.com>
2021-09-26 18:14:51 +0330

    Admin panels controller methods added

--- digikala-sample/app/Http/Controllers/AdminController.php
+++ digikala-sample/app/Http/Controllers/AdminController.php
@@ -5,6 +5,12 @@ namespace App\Http\Controllers;
 
 use App\Models\Brand;
 use App\Models\Category;
+use App\Models\Item;
+use App\Models\Order;
+use App\Models\Payment;
+use App\Models\Sale;
+use App\Models\SpecialItem;
+use App\Models\User;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
@@ -29,11 +35,24 @@ class AdminController extends Controller
     }
 
     /**
+     * Brands panel view.
+     *
+     * @return Application|Factory|View
+     */
+    public function brands()
+    {
+        $brands = Brand::query()->paginate(2);
+        return view('admin.route-views.brands')
+            ->with('title', 'brands')
+            ->with('brands', $brands->toJson(JSON_PRETTY_PRINT));
+    }
+
+    /**
      * Categories panel view.
      *
      * @return Application|Factory|View
      */
-    public function category()
+    public function categories()
     {
         $categories = Category::query()->paginate(2);
         return view('admin.route-views.categories')
@@ -46,11 +65,76 @@ class AdminController extends Controller
      *
      * @return Application|Factory|View
      */
-    public function brand()
+    public function items()
     {
-        $brands = Brand::query()->paginate(2);
-        return view('admin.route-views.brands')
-            ->with('title', 'brands')
-            ->with('brands', $brands->toJson(JSON_PRETTY_PRINT));
+        $item = Item::query()->paginate(2);
+        return view('admin.route-views.items')
+            ->with('title', 'items')
+            ->with('items', $item->toJson(JSON_PRETTY_PRINT));
+    }
+
+    /**
+     * Brands panel view.
+     *
+     * @return Application|Factory|View
+     */
+    public function specials()
+    {
+        $item = SpecialItem::query()->getRelation('item')->paginate(2);
+        return view('admin.route-views.specials')
+            ->with('title', 'items-special')
+            ->with('items', $item->toJson(JSON_PRETTY_PRINT));
+    }
+
+    /**
+     * Brands panel view.
+     *
+     * @return Application|Factory|View
+     */
+    public function users()
+    {
+        $users = User::query()->paginate(2);
+        return view('admin.route-views.users')
+            ->with('title', 'users')
+            ->with('users', $users->toJson(JSON_PRETTY_PRINT));
+    }
+
+    /**
+     * Brands panel view.
+     *
+     * @return Application|Factory|View
+     */
+    public function orders()
+    {
+        $orders = Order::query()->paginate(2);
+        return view('admin.route-views.orders')
+            ->with('title', 'orders')
+            ->with('orders', $orders->toJson(JSON_PRETTY_PRINT));
+    }
+
+    /**
+     * Brands panel view.
+     *
+     * @return Application|Factory|View
+     */
+    public function payments()
+    {
+        $payments = Payment::query()->paginate(2);
+        return view('admin.route-views.payments')
+            ->with('title', 'payments')
+            ->with('payments', $payments->toJson(JSON_PRETTY_PRINT));
+    }
+
+    /**
+     * Brands panel view.
+     *
+     * @return Application|Factory|View
+     */
+    public function sales()
+    {
+        $sales = Sale::query()->paginate(2);
+        return view('admin.route-views.sales')
+            ->with('title', 'sales')
+            ->with('sales', $sales->toJson(JSON_PRETTY_PRINT));
     }
 }
