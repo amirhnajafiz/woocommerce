af01b04154661414946ea403b152e6e54f7265c3
9831065 <najafizadeh21@gmail.com>
2021-09-26 18:19:15 +0330

    Admin navigation links set

--- digikala-sample/resources/views/components/admin/navigation.blade.php
+++ digikala-sample/resources/views/components/admin/navigation.blade.php
@@ -7,28 +7,28 @@
        class="list-group-item list-group-item-action px-5 py-3 text-center {{ request()->routeIs('admin.category') ? 'bg-primary text-white' : 'bg-white text-dark' }}">
         Categories
     </a>
-    <a href="#"
-       class="list-group-item list-group-item-action px-5 py-3 text-center {{ request()->routeIs('') ? 'bg-primary text-white' : 'bg-white text-dark' }}">
+    <a href="{{ route('admin.item') }}"
+       class="list-group-item list-group-item-action px-5 py-3 text-center {{ request()->routeIs('admin.item') ? 'bg-primary text-white' : 'bg-white text-dark' }}">
         Items
     </a>
-    <a href="#"
-       class="list-group-item list-group-item-action px-5 py-3 text-center {{ request()->routeIs('') ? 'bg-primary text-white' : 'bg-white text-dark' }}">
+    <a href="{{ route('admin.special') }}"
+       class="list-group-item list-group-item-action px-5 py-3 text-center {{ request()->routeIs('admin.special') ? 'bg-primary text-white' : 'bg-white text-dark' }}">
         Special Items
     </a>
-    <a href="#"
-       class="list-group-item list-group-item-action px-5 py-3 text-center {{ request()->routeIs('') ? 'bg-primary text-white' : 'bg-white text-dark' }}">
+    <a href="{{ route('admin.user') }}"
+       class="list-group-item list-group-item-action px-5 py-3 text-center {{ request()->routeIs('admin.user') ? 'bg-primary text-white' : 'bg-white text-dark' }}">
         Users
     </a>
-    <a href="#"
-       class="list-group-item list-group-item-action px-5 py-3 text-center {{ request()->routeIs('') ? 'bg-primary text-white' : 'bg-white text-dark' }}">
+    <a href="{{ route('admin.order') }}"
+       class="list-group-item list-group-item-action px-5 py-3 text-center {{ request()->routeIs('admin.order') ? 'bg-primary text-white' : 'bg-white text-dark' }}">
         Orders
     </a>
-    <a href="#"
-       class="list-group-item list-group-item-action px-5 py-3 text-center {{ request()->routeIs('') ? 'bg-primary text-white' : 'bg-white text-dark' }}">
+    <a href="{{ route('admin.payment') }}"
+       class="list-group-item list-group-item-action px-5 py-3 text-center {{ request()->routeIs('admin.payment') ? 'bg-primary text-white' : 'bg-white text-dark' }}">
         Payments
     </a>
-    <a href="#"
-       class="list-group-item list-group-item-action px-5 py-3 text-center {{ request()->routeIs('') ? 'bg-primary text-white' : 'bg-white text-dark' }}">
+    <a href="{{ route('admin.sale') }}"
+       class="list-group-item list-group-item-action px-5 py-3 text-center {{ request()->routeIs('admin.sale') ? 'bg-primary text-white' : 'bg-white text-dark' }}">
         Sale Codes
     </a>
     <a href="#" class="list-group-item list-group-item-action px-5 py-3 text-center bg-danger text-white">
