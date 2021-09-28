73d3af320b41986ec9874ab451915f2561205d33
9831065 <najafizadeh21@gmail.com>
2021-09-28 12:41:32 +0330

    View reset

--- digikala-sample/app/View/Components/Admin/Navigation.php
+++ digikala-sample/app/View/Components/Admin/Navigation.php
@@ -2,8 +2,15 @@
 
 namespace App\View\Components\Admin;
 
+use Closure;
+use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
+/**
+ * Class Navigation.
+ *
+ * @package App\View\Components\Admin
+ */
 class Navigation extends Component
 {
     /**
@@ -19,7 +26,7 @@ class Navigation extends Component
     /**
      * Get the view / contents that represent the component.
      *
-     * @return \Illuminate\Contracts\View\View|\Closure|string
+     * @return View|Closure|string
      */
     public function render()
     {
