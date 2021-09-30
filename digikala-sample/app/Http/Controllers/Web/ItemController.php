3853e64d212da3fdfc6bfa974ebabdbe9dca1055
9831065 <najafizadeh21@gmail.com>
2021-09-30 10:40:45 +0330

    Special items added in trait

--- digikala-sample/app/Http/Controllers/Web/ItemController.php
+++ digikala-sample/app/Http/Controllers/Web/ItemController.php
@@ -3,17 +3,15 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Web\traits\Item\Specialize;
 use App\Http\Internal\APIRequest;
 use App\Http\Requests\CreateUpdateItemRequest;
 use App\Models\Item;
-use App\Models\SpecialItem;
 use Exception;
 use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use PHPUnit\Util\Json;
 
@@ -24,6 +22,9 @@ use PHPUnit\Util\Json;
  */
 class ItemController extends Controller
 {
+    // Traits
+    use Specialize;
+
     /**
      * Display a listing of the items resource.
      *
@@ -124,56 +125,5 @@ class ItemController extends Controller
         // TODO: Photo remove from storage
 
         return redirect()->route('item.index');
-    }
-
-    /**
-     * Handles the special items view.
-     *
-     * @throws Exception
-     */
-    public function special()
-    {
-        $items = Json::prettify(APIRequest::handle(route('api.all.special')));
-
-        return view('admin.route-views.specials')
-            ->with('items', $items)
-            ->with('title', '-special-items');
-    }
-
-    /**
-     * Handles the special making.
-     *
-     * @param Request $request
-     * @param Item $item
-     * @return Application|ResponseFactory|Response
-     */
-    public function makeSpecial(Request $request, Item $item)
-    {
-        // TODO: Validate request
-
-        SpecialItem::query()->create([
-            'item_id' => $item->id,
-            'expire_date' => now()->addMonth(),
-            'discount' => $request->get('amount')
-        ]);
-
-        // TODO: Use a json response
-        return response();
-    }
-
-    /**
-     * Handles the special item removing.
-     *
-     * @param Item $item
-     * @return Application|ResponseFactory|Response
-     */
-    public function removeSpecial(Item $item)
-    {
-        SpecialItem::query()
-            ->where('item_id', '=', $item->id)
-            ->delete();
-
-        // TODO: Use a json response
-        return response();
     }
 }
