26fda427b1d3d357e43a19ecdb5e0ac5557afb80
9831065 <najafizadeh21@gmail.com>
2021-09-30 10:14:50 +0330

    User controller web routes added

--- digikala-sample/routes/web.php
+++ digikala-sample/routes/web.php
@@ -35,3 +35,13 @@ Route::resource('category', \App\Http\Controllers\Web\CategoryController::class)
 // Admin routes
 Route::get('/admin', [\App\Http\Controllers\User\AdminController::class, 'index'])
     ->name('admin.panel');
+
+// User routes
+Route::post('login', [\App\Http\Controllers\User\UserController::class, 'login'])
+    ->name('user.login');
+
+Route::post('register', [\App\Http\Controllers\User\UserController::class, 'register'])
+    ->name('user.register');
+
+Route::post('logout', [\App\Http\Controllers\User\UserController::class, 'logout'])
+    ->name('user.logout');
