597b990b3182174264ac8ec9761daa5d5c026246
9831065 <najafizadeh21@gmail.com>
2021-10-02 09:48:12 +0330

    Welcome view set

--- digikala-sample/resources/views/welcome.blade.php
+++ digikala-sample/resources/views/welcome.blade.php
@@ -1,1 +1,20 @@
-
+<x-app-layout>
+    @foreach($items as $item)
+        <div class="card">
+            <img class="card-img-top" src="{{ $item->image->path }}" alt="{{ $item->image->alt }}" />
+            <div class="card-body">
+                <h5 class="card-title">
+                    {{ $item->name }}
+                </h5>
+                <p class="card-text">
+                    {{ $item->brand->name }}
+                </p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">
+                    Price: {{ $item->price }}$
+                </small>
+            </div>
+        </div>
+    @endforeach
+</x-app-layout>
