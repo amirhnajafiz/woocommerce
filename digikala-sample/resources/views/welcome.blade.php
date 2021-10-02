ca89db9fa28c62aef0e253fd601ee3bee21667ee
9831065 <najafizadeh21@gmail.com>
2021-10-02 10:22:53 +0330

    Welcome view done

--- digikala-sample/resources/views/welcome.blade.php
+++ digikala-sample/resources/views/welcome.blade.php
@@ -1,22 +1,26 @@
 <x-app-layout>
     @section('title', $title)
-    @foreach($items as $item)
-        <div class="card w-25 mx-auto my-5">
-            <img class="card-img-top" src="{{ $item->image->path }}" alt="{{ $item->image->alt }}" />
-            <div class="card-body">
-                <h5 class="card-title">
-                    {{ $item->name }}
-                </h5>
-                <p class="card-text">
-                    {{ $item->brand->name }}
-                </p>
+    <div class="row m-0 w-75 m-auto">
+        @foreach($items as $item)
+            <div class="col-2 card mx-auto my-5">
+                <img class="card-img-top" src="{{ $item->image->path }}" alt="{{ $item->image->alt }}" />
+                <div class="card-body">
+                    <h5 class="card-title">
+                        {{ $item->name }}
+                    </h5>
+                    <p class="card-text">
+                        {{ $item->brand->name }}
+                    </p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-muted">
+                        Price: {{ $item->price }}$
+                    </small>
+                </div>
             </div>
-            <div class="card-footer">
-                <small class="text-muted">
-                    Price: {{ $item->price }}$
-                </small>
-            </div>
-        </div>
-    @endforeach
-    {{ $items->links() }}
+        @endforeach
+    </div>
+    <div class="my-3">
+        {{ $items->links() }}
+    </div>
 </x-app-layout>
