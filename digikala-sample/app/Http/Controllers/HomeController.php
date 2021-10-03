530c379bcbfe5b623ca3a4421cd031da65986a3b
9831065 <najafizadeh21@gmail.com>
2021-10-03 17:46:30 +0330

    Front refactor

--- digikala-sample/app/Http/Controllers/HomeController.php
+++ digikala-sample/app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Internal\APIRequest;
 use App\Models\Item;
+use App\Models\SpecialItem;
 use Exception;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
@@ -28,9 +29,11 @@ class HomeController extends Controller
     public function index(string $filter='id', string $mode='asc')
     {
         $items = Item::query()->orderBy($filter, $mode)->paginate(5);
+        $specials = SpecialItem::query()->get()->random(5);
 
         return view('welcome')
             ->with('title', 'home')
+            ->with('specials', $specials)
             ->with('items', $items);
     }
 
--- digikala-sample/resources/views/welcome.blade.php
+++ digikala-sample/resources/views/welcome.blade.php
@@ -1,63 +1,59 @@
 <x-app-layout>
-    <div class="bg-white d-flex justify-evenly py-2">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            <a href="{{ route('home', 'id') }}">
-                Normal
-            </a>
-        </h2>
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            <a href="{{ route('home', ['price', 'desc']) }}">
-                High Price
-            </a>
-        </h2>
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            <a href="{{ route('home', ['score', 'desc']) }}">
-                High Score
-            </a>
-        </h2>
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            <a href="{{ route('home', 'price') }}">
-                Low Price
-            </a>
-        </h2>
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            <a href="{{ route('home', 'score') }}">
-                Low Score
-            </a>
-        </h2>
-    </div>
-
     @section('title', $title)
-    <div class="row m-0 w-75 m-auto">
+    <div class="row m-0 w-75 m-auto my-3">
         <h1>
             Special items
         </h1>
-        @foreach($special_items as $item)
+        @foreach($specials as $special)
             <div class="col-2 card mx-auto my-5">
-                <img class="card-img-top" src="{{ $item->image->path }}" alt="{{ $item->image->alt }}" />
+                <img class="card-img-top" src="{{ $special->item->image->path }}" alt="{{ $special->item->image->alt }}" />
                 <div class="card-body">
                     <h5 class="card-title">
-                        {{ $item->name }}
+                        {{ $special->item->name }}
                     </h5>
                     <p class="card-text">
-                        {{ $item->brand->name }}
+                        {{ $special->item->brand->name }}
                     </p>
                 </div>
                 <div class="card-footer">
                     <small class="text-muted">
-                        Price: {{ $item->price }}$
+                        Price: {{ $special->item->price }}$
                     </small>
                 </div>
             </div>
         @endforeach
     </div>
-    <div class="my-3">
-        {{ $special_items->links() }}
-    </div>
     <div class="row m-0 w-75 m-auto">
         <h1>
             Items
         </h1>
+        <div class="bg-white d-flex justify-evenly py-2">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                <a href="{{ route('home', 'id') }}">
+                    Normal
+                </a>
+            </h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                <a href="{{ route('home', ['price', 'desc']) }}">
+                    High Price
+                </a>
+            </h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                <a href="{{ route('home', ['score', 'desc']) }}">
+                    High Score
+                </a>
+            </h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                <a href="{{ route('home', 'price') }}">
+                    Low Price
+                </a>
+            </h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                <a href="{{ route('home', 'score') }}">
+                    Low Score
+                </a>
+            </h2>
+        </div>
         @foreach($items as $item)
             <div class="col-2 card mx-auto my-5">
                 <img class="card-img-top" src="{{ $item->image->path }}" alt="{{ $item->image->alt }}" />
