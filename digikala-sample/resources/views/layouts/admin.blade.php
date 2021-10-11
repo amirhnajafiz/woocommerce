871961a073e40daac6923920cd31d632b12b7612
9831065 <najafizadeh21@gmail.com>
2021-10-11 19:49:31 +0330

    Admin panel refactor in view

--- digikala-sample/resources/views/layouts/admin.blade.php
+++ digikala-sample/resources/views/layouts/admin.blade.php
@@ -14,13 +14,34 @@
         <!-- Styles -->
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
+        <style>
+            body {
+                background: linear-gradient(-45deg, #ffffff, #a2a2a2, #707070, #000000);
+                background-size: 400% 400%;
+                animation: gradient 20s ease infinite;
+                height: 100vh;
+            }
+
+            @keyframes gradient {
+                0% {
+                    background-position: 0 50%;
+                }
+                50% {
+                    background-position: 100% 50%;
+                }
+                100% {
+                    background-position: 0 50%;
+                }
+            }
+        </style>
+
         @yield('style')
 
         <title>
             {{ "Digikala-" . $title }}
         </title>
     </head>
-    <body class="bg-light">
+    <body>
         <div class="container-fluid">
             @yield('content')
         </div>
