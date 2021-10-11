a17d02cb4608b439f9c80bec1db34bd0b6c6549e
9831065 <najafizadeh21@gmail.com>
2021-10-11 18:55:48 +0330

    Added funzzy background

--- digikala-sample/resources/views/layouts/admin.blade.php
+++ digikala-sample/resources/views/layouts/admin.blade.php
@@ -5,8 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Digikala @yield('title')</title>
-
         <!-- Bootstrap CDN -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
 
