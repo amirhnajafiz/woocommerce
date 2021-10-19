c1e657790d4621abd15538847ea987418bdd5edb
9831065 <najafizadeh21@gmail.com>
2021-10-19 21:20:54 +0330

    Cart controller view linked correctly

--- digikala-sample/app/Http/Controllers/CartController.php
+++ digikala-sample/app/Http/Controllers/CartController.php
@@ -7,6 +7,7 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * Class CartController for managing user carts.
@@ -22,7 +23,8 @@ class CartController extends Controller
      */
     public function index(): View
     {
-        return view('web.user.cart');
+        return view('utils.cart.index')
+            ->with('user', Auth::user());
     }
 
     /**
--- digikala-sample/resources/views/utils/cart/index.blade.php
+++ digikala-sample/resources/views/utils/cart/index.blade.php
@@ -1,11 +1,11 @@
-@section('title', __('-cart-', \Illuminate\Support\Facades\Auth::user()->name))
+@section('title', __('-cart-', $user->name))
 
 <x-app-layout>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    This is your cart, <strong>{{ \Illuminate\Support\Facades\Auth::user()->name }}</strong>
+                    This is your cart, <strong>{{ $user->name }}</strong>
                 </div>
             </div>
         </div>
